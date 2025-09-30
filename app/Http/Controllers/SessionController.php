0278068917e9962cfa6fc47da3369e61c39c8e89
ist14k <istiak_akash@outlook.com>
2025-09-30 11:23:11 +0600

    Implement user registration and login functionality with corresponding views

---
+++ app/Http/Controllers/SessionController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionController extends Controller
+{
+    public function show()
+    {
+        return view('auth.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $data = $request->validate([
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if (Auth::attempt($data)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('todo.index');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
+
+    public function destroy(Request $request)
+    {
+        // Logic for logging out the user
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,24 @@
 <?php
 
+use App\Http\Controllers\RegistrationController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->route('todo.index');
-});
+})->name('home');
 
 Route::get('/todo', function () {
     return view('todo.index');
-})->name('todo.index');
+})->name('todo.index')->middleware('auth');
+
+Route::get('/todo/{id}', function ($id) {
+    return view('todo.show', ['id' => $id]);
+})->name('todo.show')->middleware('auth');
+
+Route::get('/register', [RegistrationController::class, 'show'])->name('register');
+Route::post('/register', [RegistrationController::class, 'register'])->name('register.register');
+
+Route::get('/login', [SessionController::class, 'show'])->name('login');
+Route::post('/login', [SessionController::class, 'authenticate'])->name('login.authenticate');
+Route::post('/logout', [SessionController::class, 'destroy'])->name('logout');
