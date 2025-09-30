252c6a9f52be689a96325801d64d1f33922d246c
ist14k <istiak_akash@outlook.com>
2025-09-30 22:12:13 +0600

    Add Note and Task models, controllers, factories, and migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Note;
+use App\Models\Task;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +15,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create()->each(function ($user) {
+            Task::factory(random_int(5, 10))->create(['user_id' => $user->id])->each(function ($task) {
+                Note::factory(random_int(3, 5))->create(['task_id' => $task->id, 'user_id' => $task->user_id]);
+            });
+        });
 
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ])->each(function ($user) {
+            Task::factory(5)->create(['user_id' => $user->id])->each(function ($task) use ($user) {
+                Note::factory(3)->create(['task_id' => $task->id, 'user_id' => $user->id]);
+            });
+        });
     }
 }
